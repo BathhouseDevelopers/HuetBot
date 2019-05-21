f7b5be082ac15dd5344fabc851d53dc3e71d1d67
sdavydovv <lppt@inbox.ru>
2019-05-21 20:08:36 +0300

    d

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ chats = {filter: false,
 		 group_dev1: '-248416559',
 		 chat_sergey: '309739368',
 		 group_bania: '-187427483'}
-chats.list = [chats.group_dev1, chats.chat_sergey, chats.group_bania] //,chats.group_bania
+chats.list = [chats.group_dev1, chats.chat_sergey] //,chats.group_bania
 
 
 PARAMS = {skipRandom:15, responseTimeout:3, skipBetweenMessages:3}
